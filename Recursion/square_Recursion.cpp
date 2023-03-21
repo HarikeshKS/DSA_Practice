2d48c5db73e7b0e6e75ca0565a9f040f035a5ac9
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2023-03-21 11:08:57 +0530

    Exploring more on Recursion.

--- Recursion/square_Recursion.cpp
+++ Recursion/square_Recursion.cpp
@@ -13,13 +13,28 @@ int pow(int x, int n)
     }
 }
 
-int main()
+// int main()
+// {
+//     int x, n;
+//     cout << "Enter the number- ";
+//     cin >> x;
+//     cout << "Enter the power for the number- ";
+//     cin >> n;
+//     cout << "Result: " << pow(x, n) << endl;
+//     return 0;
+// }
+
+// This is for printing n natural numbers using recursion.
+
+int num(int n){
+    if(n==0){
+        return 0;
+    }
+    return n+num(n+1);
+}
+int main(int argc, char const *argv[])
 {
-    int x, n;
-    cout << "Enter the number- ";
-    cin >> x;
-    cout << "Enter the power for the number- ";
-    cin >> n;
-    cout << "Result: " << pow(x, n) << endl;
+    int n;
+
     return 0;
 }
