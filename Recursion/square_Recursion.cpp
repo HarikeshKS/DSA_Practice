d1eea14ca823b5d95a642fc6c49f1813cd97094b
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2023-03-23 09:39:36 +0530

    Added a code about printing natural numbers using Recursion.

--- Recursion/square_Recursion.cpp
+++ Recursion/square_Recursion.cpp
@@ -26,20 +26,35 @@ int pow(int x, int n)
 
 // This is for printing sum of n natural numbers using recursion.
 
-int num(int n){
-    if(n==0){
+// int num(int n){
+//     if(n==0){
+//         return 0;
+//     }
+//     return n+num(n-1);
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     int res=num(n);
+//     cout<<"Sum of first 5 natural number is: "<<res<<endl;
+//     return 0;
+// }
+int printnum(int n,int a=1){
+    if (a>n)
+    {
         return 0;
     }
-    return n+num(n-1);
+    cout<<a<<endl;
+    printnum(n, a+1);
 }
+
 int main(int argc, char const *argv[])
 {
     int n;
-    cout<<"Enter the number: ";
+    cout<<"Enter the number upto which you want to print: ";
     cin>>n;
-
-    int res=num(n);
-
-    cout<<"Sum of first 5 natural number is: "<<res<<endl;
+    int c=printnum(n);
     return 0;
 }
